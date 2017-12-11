f4d372dc31e7c20c45db42e7b4a0f3c29be1c5c2
RH <89388@MCH10-297-001.edu01.education-siemens.net>
2017-12-11 15:44:35 +0100

    DB libs

---
+++ src/java/Verwaltung/DBconn.java
@@ -0,0 +1,35 @@
+package Verwaltung;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 89388
+ */
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconn {
+    
+    // createNewDatabase("test.db");
+    public static void createNewDatabase(String fileName) {
+        String url = "jdbc:sqlite:sqlite/" + fileName;
+ 
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : index
+    Created on : 11.12.2017, 15:16:22
+    Author     : 89388
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <%
+            Verwaltung.DBconn.createNewDatabase("test.db");
+        %>
+    </body>
+</html>
